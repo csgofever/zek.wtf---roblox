a67d53c1b42dcc65c9060faee8732b1225f9892d
csgofever <zioncrishan@gmail.com>
2022-03-13 14:35:05 +0800

    e

---
+++ packages/modids.lua
@@ -0,0 +1,33 @@
+
+--// put the player's user id in this format "[userid] = true;"
+--// refresh the page before editing
+--// dab
+
+MODS = {
+    [2464548888] = true; -- fever_vfx (owner)
+}
+
+ADMIN = {
+    [2464548888] = true; -- fever_vfx (owner)
+}
+
+--// Don't mess with anything below this
+function initiateNames()
+	game.Players.LocalPlayer.Character.UpperTorso:FindFirstChild('OriginalSize'):Destroy()
+	for _,v in pairs(game:GetService('Players'):GetPlayers()) do
+		if v.Character then
+			if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
+			end
+			if MODS[v.UserId] then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
+			end
+			if ADMIN[v.UserId] then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
+			end
+		end
+	end
+end
+
+local successful, errored = pcall(initiateNames)
+return MODS
